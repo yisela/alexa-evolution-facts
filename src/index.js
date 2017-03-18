72b83e311660caa118f96dabf3c5b1b812b9fbd8
Yisela <utopica@gmail.com>
2017-03-18 16:54:44 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,23 +6,18 @@ var languageStrings = {
     "en": {
         "translation": {
             "FACTS": [
-                "A year on Mercury is just 88 days long.",
-                "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-                "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
-                "On Mars, the Sun appears about half the size as it does on Earth.",
-                "Earth is the only planet not named after a god.",
-                "Jupiter has the shortest day of all the planets.",
-                "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-                "The Sun contains 99.86% of the mass in the Solar System.",
-                "The Sun is an almost perfect sphere.",
-                "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-                "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-                "The temperature inside the Sun can reach 15 million degrees Celsius.",
-                "The Moon is moving approximately 3.8 cm away from our planet every year."
+                "Some snakes have hipbones, which shows they once had four legs like their close cousins the lizards.",
+                "Birds and dinosaurs both evolved from reptiles. The closest living reptilian relation of a bird is the crocodile.",
+                "Until about 10,000 years ago, most people had brown eyes. A single genetic mutation originating in the Black Sea is responsible for the current 8% of the world’s population that has blue eyes.",
+                "All humans develop a tail in the womb that eventually dissolves.",
+                "The phrase 'Survival of the Fittest' is not Darwin's. It was actually oined by the philosopher Herbert Spencer.",
+                "Darwin never said that humans come from monkeys. His words actually quote a hairy, tailed, quadruped and probably arboreal ancestor.",
+                "Humans share about 31% of their genes with yeast, and about 50% with bananas.",
+                "WIP!!"
             ],
-            "SKILL_NAME" : "Space Facts",
+            "SKILL_NAME" : "Evolution Facts",
             "GET_FACT_MESSAGE" : "Here's your fact: ",
-            "HELP_MESSAGE" : "You can say tell me a space fact, or, you can say exit... What can I help you with?",
+            "HELP_MESSAGE" : "You can say tell me an evolution fact, or, you can say exit... What can I help you with?",
             "HELP_REPROMPT" : "What can I help you with?",
             "STOP_MESSAGE" : "Goodbye!"
         }
@@ -44,7 +39,7 @@ var languageStrings = {
                 "The temperature inside the Sun can reach 15 million degrees Celsius.",
                 "The Moon is moving approximately 3.8 cm away from our planet every year."
             ],
-            "SKILL_NAME" : "American Space Facts"
+            "SKILL_NAME" : "American Evolution Facts"
         }
     },
     "en-GB": {
@@ -64,7 +59,7 @@ var languageStrings = {
                 "The temperature inside the Sun can reach 15 million degrees Celsius.",
                 "The Moon is moving approximately 3.8 cm away from our planet every year."
             ],
-            "SKILL_NAME" : "British Space Facts"
+            "SKILL_NAME" : "British Evolution Facts"
         }
     },
     "de": {
